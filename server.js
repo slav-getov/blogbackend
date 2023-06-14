63ca81178be63837650557dd8886559632913588
Preslav Getov <preslavgetov@preslavs-mbp.m-a.local>
2023-06-14 10:51:42 +0300

    testing connection

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const pgp = require("pg-promise");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const USERNAME = process.env.USERNAME;
-console.log(USERNAME);
-// const PASSWORD =
-// const DBNAME =
-// const db = pgp('postgres://john:pass123@localhost:5432/products');
+const PASSWORD = process.env.PASSWORD;
+const DBNAME = process.env.DBNAME;
+const db = pgp(`postgres://${USERNAME}:${PASSWORD}@localhost:5432/${DBNAME}`);
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.send("Hello World!");
+  db.one("SELECT $1 AS value", 123)
+    .then((data) => {
+      console.log("DATA:", data.value);
+    })
+    .catch((error) => {
+      console.log("ERROR:", error);
+    });
 });
 
 app.listen(PORT, () => {
