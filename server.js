dde5de72ebb8fd6ebfaf9d227712aeccd0f22707
Preslav Getov <preslavgetov@preslavs-mbp.m-a.local>
2023-06-14 11:10:10 +0300

    connection works

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const USERNAME = process.env.USERNAME;
 const PASSWORD = process.env.PASSWORD;
 const DBNAME = process.env.DBNAME;
 const db = pgp(`postgres://${USERNAME}:${PASSWORD}/${DBNAME}`);
-
+//connection works
 function getDbName() {
   return db.$cn;
 }
 app.get("/", async (req, res) => {
-  res.send(getDbName());
+  //this line now works. Ofc we will not return full db info because now a simple curl gets it.
+  //res.send(getDbName());
 });
 
 app.listen(PORT, () => {
