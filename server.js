c8c342155ea4e9c444680ee0bb7e6d4144f92893
Preslav Getov <preslavgetov@preslavs-mbp.m-a.local>
2023-06-14 10:43:42 +0300

    testing env vars

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const pgp = require("pg-promise");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+const USERNAME = process.env.USERNAME;
+console.log(USERNAME);
+// const PASSWORD =
+// const DBNAME =
+// const db = pgp('postgres://john:pass123@localhost:5432/products');
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
